d1ad509cd9c4b82f3097b85ce763f737ca36c753
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-12 00:10:41 +0530

    taking boolean input from user

--- Basics/user_input.py
+++ Basics/user_input.py
@@ -1,9 +1,18 @@
 # we can take input from user using input function in python
 
 # we can take string input as follows
-name = input("What is your name ?")
-print("hello "+name)
+# name = input("What is your name ?")
+# print("hello "+name)
 
-# int value as an input
-age = int(input("Enter your age : "))
-print("your age is "+ str(age)) # we have to convert int to string for concatination
+# # int value as an input
+# age = int(input("Enter your age : "))
+# print("your age is "+ str(age)) # we have to convert int to string for concatination
+
+# float
+# height = float(input("Enter your Height : "))
+# print("Height = " + str(height));
+
+
+# boolean
+boolean = bool(input("Enter True if your Male and False if Female"));
+print(boolean);
