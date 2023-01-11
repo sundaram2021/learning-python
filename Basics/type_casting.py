262da24781b724c3f550649c1f19068bb80e5a7e
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-12 00:05:10 +0530

    type casting in python to convert fro one data type to another

---
+++ Basics/type_casting.py
@@ -0,0 +1,11 @@
+# type - casting = convert the data type of a value to another data type
+# NOTE : concatination is only possible with strings
+
+a = 1;   #int
+b = 2.6;  #float
+z = "3"  #str
+
+print(int(b)) # b = 2
+print(str(a)) # a = '1'
+print(float(a)) # a = 1.0
+print(int(z))  # z = 3
