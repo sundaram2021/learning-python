6d24148cf43a762d29479727991789b26c5c359e
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-11 17:07:23 +0530

    this file is created to learn about various string methods in python

---
+++ Basics/string_methods.py
@@ -0,0 +1,38 @@
+'''
+  In this file we will learn various string methods
+  in python.
+
+'''
+
+
+name = "sundaram"
+
+# len() return the length of the strings
+# print(len(name))
+
+# find() returns the index of string passed into it
+# print(name.find("d"))
+
+# capitalize the first letter of your string
+# print(name.capitalize())
+
+# change your whole string in uppercase
+# print(name.upper())
+
+# change your whole string in lowercase
+# print(name.lower())
+
+# returns true if string is a number and false if not
+# name = "123"
+# print(name.isdigit());
+
+#return true is whole string contains alphabetical characters
+# name = "sundaram jha"
+# print(name.isalpha())
+
+# count() function returns how many times character repeats
+# print(name.count('a'))
+
+#replaces() function replace the first character with the second character
+# print(name.replace("a", "A"))
+
