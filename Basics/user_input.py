5476881b6fd9a2ce8756a95827c150b541de2b62
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-12 00:05:52 +0530

    how to take input from user in python

---
+++ Basics/user_input.py
@@ -0,0 +1,9 @@
+# we can take input from user using input function in python
+
+# we can take string input as follows
+name = input("What is your name ?")
+print("hello "+name)
+
+# int value as an input
+age = int(input("Enter your age : "))
+print("your age is "+ str(age)) # we have to convert int to string for concatination
