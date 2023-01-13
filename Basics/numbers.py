f8a7f1fcf94ce33c0277f0a50f31f774f9f80f8b
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-13 18:33:46 +0530

    adding math module and various functions in the basket

---
+++ Basics/numbers.py
@@ -0,0 +1,15 @@
+import math
+
+pi = 3.14
+a = 2
+b = 3
+c = 9
+
+#various functions related with numbers
+# print(round(pi))
+# print(math.ceil(pi))/
+# print(abs(pi))
+# print(pow(pi, 2))
+# print(math.sqrt(pi))
+# print(max(a, b, c, pi))
+print(min(a, b, c, pi))
