1034a06d6d41ea2ddb99e4d95c76ec8a8ce0c002
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-13 22:45:33 +0530

    added tuples in learning path

---
+++ Basics/tuples.py
@@ -0,0 +1,13 @@
+# tuple = collection which is ordered and unchangeable
+#         used to group together related data
+
+student = ("Sundaram", 20, "male");
+
+# print(student.count("Sundaram"))
+# print(student.index("male"))
+
+# for i  in student:
+#     print(i)
+
+# if "male" in student:
+#     print("its male")    
