e55e04ca9f9681358d30386a7c4b86a5a1f2ebc6
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-14 01:59:04 +0530

    adding modular programming

---
+++ Basics/modular_programming.py
@@ -0,0 +1,13 @@
+# module = a file containing python code. May contain function, classes etc
+#          used in modular programming , which is to separate a program into parts
+
+
+# import modular_programming2 as md
+
+# md.hello()
+
+# md.bye()
+
+help("modules")
+
+
---
+++ Basics/modular_programming2.py
@@ -0,0 +1,7 @@
+
+
+def hello():
+    print("Hello dude!!!")
+
+def bye():
+    print("Bye! Have a beautiful day and good journey ahead");    
