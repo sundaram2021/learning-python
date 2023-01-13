19f73db47a82298c3c173ba354d03fac6d0baed6
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-13 22:54:49 +0530

    added sets

---
+++ Basics/set.py
@@ -0,0 +1,17 @@
+# set  = collection ehich is unordered, unindexed. No duplicate values
+
+utensils = {"fork", "spoon", "knife"}
+dishes = {"bowl", "plate", "cup", "knife"}
+
+# utensils.add("napkin")
+# utensils.remove("fork")
+# utensils.clear()
+# utensils.update(dishes)
+# print(dishes[0])  # not possible
+# dinner_tabel = utensils.union(dishes)
+
+# print(utensils.difference(dishes))
+# print(utensils.intersection(dishes))
+
+# for s in dinner_tabel:
+#     print(s)
