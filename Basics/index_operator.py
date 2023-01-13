c039f3da5a0dc39f3e772cb5ad89dcdb043037a3
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-13 23:22:17 +0530

    index operator in python

---
+++ Basics/index_operator.py
@@ -0,0 +1,11 @@
+# index operator [] = give access to a sequence's element (str, list, tuples)
+
+name = "sundaram!"
+
+# if  (name[0].isupper()):
+#     name = name.capitalize()
+
+first_name = name[:3].upper()
+last_letter = name[-1]
+
+print(last_letter)
