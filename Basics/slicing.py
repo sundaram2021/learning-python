2a35e8e4f1c274673de68f3956f7b171322f2f61
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-13 18:55:57 +0530

    slicing using indexing and slice function

---
+++ Basics/slicing.py
@@ -0,0 +1,23 @@
+# slicing = create a substring by extracting from
+#  another string indexing[] or slice() [start:stop:step]
+
+# name = "Sundaram"
+
+# #here 0 is inclusive and 3 is exclusive
+# substr1 = name[0:3]
+# substr2 = name[:3]
+# substr3 = name[4:]
+# substr4 = name[1:9:2]
+# substr5 = name[::3]
+# reverse = name[::-1]
+
+# print(reverse)
+
+
+# using slice() frunction
+# [start,stop,step]
+
+
+# website = "https://google.com"
+# slice = slice(3, 13, 3)
+# print(website[slice])
