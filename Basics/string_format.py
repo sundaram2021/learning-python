5914958581f0ea900dc46da3ae5015e11c3ec1bc
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-14 00:40:54 +0530

    added string formatter

---
+++ Basics/string_format.py
@@ -0,0 +1,12 @@
+'''
+str.format() = optional method that gives users more
+control when displying output
+
+'''
+
+animal = "cow"
+item = "moon"
+
+#print("The "+ animal + " jumped over the moon " + item)
+print(f"The {animal} jumped over the {item}")
+# print("The {} jumped over the {}".format(animal, item))
