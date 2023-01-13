cee1b53aa7f3491cc387713ea9faabcf9a479e63
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-13 22:35:43 +0530

    talking about the list

---
+++ Basics/list.py
@@ -0,0 +1,17 @@
+# list = used to store multiple items in the single variable
+#        may or may not of same data type
+
+food = ["pizza", "burger", "Biryani", "Butter chicken", "Fish"]
+
+# functions in list
+# food.append("naan")
+#food.count("pizza")
+# food.pop(4)
+# food.insert(0, "Mutton")
+# food.sort()
+# food.remove("pizza")
+# food.clear()
+
+
+for i in food:
+    print(i)
